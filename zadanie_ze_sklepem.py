4e9aeebad8686adfb0956a3af4caf9136c6362aa
Marcin Uciński <mucinski@poczta.onet.pl>
2022-03-28 14:47:28 +0200

    add dict

--- zadanie_ze_sklepem.py
+++ zadanie_ze_sklepem.py
@@ -2,10 +2,3 @@ lista_zakupów = {
     'piekarnia': ['Chleb', 'Pączek', 'Bułki'],
     'warzywniak': ['Marchew', 'Seler', 'Rukola']
     }
-for key in lista_zakupów:
-    print('Idę do', key, 'i kupuję tu następujące rzeczy: ', lista_zakupów[key])
-    count = 0
-for x in lista_zakupów:
-    if isinstance(lista_zakupów[x], list):
-        count += len(lista_zakupów[x])
-print('W sumie kupuję', count, 'produktów')
