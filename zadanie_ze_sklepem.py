3986e9db4a0eb20ab12799e9249f68c597f9787d
Marcin Uciński <mucinski@poczta.onet.pl>
2022-03-28 14:56:28 +0200

    add counting, not upper yet

--- zadanie_ze_sklepem.py
+++ zadanie_ze_sklepem.py
@@ -4,3 +4,8 @@ lista_zakupów = {
     }
 for key in lista_zakupów:
     print('Idę do', key, 'i kupuję tu następujące rzeczy: ', lista_zakupów[key])
+count = 0
+for x in lista_zakupów:
+    if isinstance(lista_zakupów[x], list):
+        count += len(lista_zakupów[x])
+print('W sumie kupuję', count, 'produktów')
