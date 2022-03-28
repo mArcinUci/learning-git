474b56b88d6ad2f69049d9ede24ab1c4e01a9d47
Marcin Uciński <mucinski@poczta.onet.pl>
2022-03-28 14:50:39 +0200

    add for

--- zadanie_ze_sklepem.py
+++ zadanie_ze_sklepem.py
@@ -2,3 +2,5 @@ lista_zakupów = {
     'piekarnia': ['Chleb', 'Pączek', 'Bułki'],
     'warzywniak': ['Marchew', 'Seler', 'Rukola']
     }
+for key in lista_zakupów:
+    print('Idę do', key, 'i kupuję tu następujące rzeczy: ', lista_zakupów[key])
